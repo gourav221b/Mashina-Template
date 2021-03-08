cda3e6e6968f3f17f28a16db114dbe2914fa5e80
gourav221b <57912157+gourav221b@users.noreply.github.com>
2021-03-08 23:32:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,9 +22,10 @@ function App() {
     {/* Services */}
     {/* offer with grid */}
     {/* Testimonials */}
+    <Route path="*" component={Missing} />
     </Switch>
   <Footer/>
-  <Route path="*" component={Missing} />
+
      </Router>
     </div>
   );
